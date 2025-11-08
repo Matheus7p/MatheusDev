ec16e0021791fac73e3392d08862592ffd2996b7
Matheus7p <matheus.costa.7p@gmail.com>
2025-11-08 16:10:34 -0300

    fix: whitespace-nowrap

--- src/components/about.component.tsx
+++ src/components/about.component.tsx
@@ -4,7 +4,7 @@ import SocialButtons from "./socialButton";
 export const About = () => {
     return (
         <>
-          <h1 className="text-4xl text-[#f5f5f5b7] font-medium mb-12 leading-[1.4] whitespace-nowrap">Desenvolvedor Web Full-Stack</h1>
+          <h1 className="text-4xl text-[#f5f5f5b7] font-medium mb-12 leading-[1.4] md:whitespace-nowrap">Desenvolvedor Web Full-Stack</h1>
           <p className="text-[#a3a3a3] font-normal text-md mt-6 mb-8 leading-6 text-justify">
             Proficiente em desenvolvimento web. Possuo domínio de tecnologias front‑end como <span className="text-[#f5f5f5]">React.js</span> e seu ecossistema, além de experiência no desenvolvimento de back‑end com <span className="text-[#f5f5f5]">Node.js</span> e <span className="text-[#f5f5f5]">TypeScript</span>
           </p>
