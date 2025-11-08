6734bbc84c0bf0d6e51cba234e0f2b32564da181
Matheus7p <matheus.costa.7p@gmail.com>
2025-11-08 05:58:56 -0300

    feat: add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import Cursor from "./components/cursor.component";
+import Header from "./components/header.component";
 import { CursorProvider } from "./context/cursor.ctx";
 
 function App() {
   return (
     <CursorProvider>
-      <Cursor />
-        <main className="">
-          <button>teste</button>
+      <div className="min-h-[calc(100vh-48px)] sm:min-h-[calc(100vh-96px)] flex flex-col">
+        <Cursor />
+        <main className="mx-auto p-6 w-full">
+          <Header />
         </main>
+      </div>
     </CursorProvider>
   );
 }
---
+++ src/components/header.component.tsx
@@ -0,0 +1,20 @@
+const Header = () => {
+  return (
+    <div className="flex justify-between items-center mb-16">
+      <div className="flex items-center gap-4">
+        <a href="/" className="cursor-pointer">
+          <img
+            src="matheus.jpg"
+            className="h-12 w-12 rounded-full shadow-[#5A32C8_-3px_0px_4px_0px] hover:scale-110 duration-200"
+          />
+        </a>
+        <div className="flex flex-col">
+          <p className="text-[#f5f5f5] font-bold text-md">Matheus Costa</p>
+          <p className="text-[#a3a3a3] font-normal text-sm">São Paulo - SP</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
