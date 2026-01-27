c710da5e348db78a53933e7e8869f9d18c48348d
Matheus7p <matheus.costa.7p@gmail.com>
2026-01-27 02:43:41 -0300

    feat: experience

---
+++ src/constans/experience.ts
@@ -0,0 +1,14 @@
+import { ExperienceType } from "../types/experience";
+
+export const experiences: ExperienceType[] = [
+  {
+    id: 1,
+    company: "Match House",
+    companyUrl: "https://www.matchhouse.co/",
+    designation: "Desenvolvedor Freelancer",
+    startDate: "Março, 2024",
+    endDate: "o momento",
+    description:
+      "Desenvolvedor Front-end, Atuando sob a supervisão de um desenvolvedor sênior desenvolvi uma plataforma imobiliária escalável com Next.js e TypeScript",
+  },
+];
